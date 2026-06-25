7c643e4e5114622d8ea6c2bb38b43ce1b834b8b6
Nafidah Mauna <nafidahmauna@gmail.com>
2026-06-25 18:35:41 +0400

    Implement functions to open WA and email
    Added functions to open WhatsApp and email for inquiries.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("SkillBridge UAE Loaded");
+function openWA() {
+  window.open(
+    "https://wa.me/971553991840?text=Hi%20I%20need%20help%20with%20SkillBridge%20UAE",
+    "_blank"
+  );
+}
+
+function openEmail() {
+  window.location.href =
+    "mailto:nafidahmauna@gmail.com?subject=SkillBridge Inquiry";
+}
